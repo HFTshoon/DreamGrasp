cfd13a1d6aefe329cbe4ea00eec3543df89fc95f
HFTshoon <shoon0602@snu.ac.kr>
2025-01-28 11:48:07 +0000

    improve gen idx selection

--- imggen/gen_zeronvs.py
+++ imggen/gen_zeronvs.py
@@ -48,18 +48,74 @@ def load_gen_model(output_dir, model_dir, model_iteration, device):
 def choose_idx_to_generate(seq_info, known_area_ratio):
     generate_cnt = min(2 ** seq_info.cur_stage, seq_info.target_poses_cnt - seq_info.generated_poses_cnt)
     min_known_area_ratio = 0.5
+    max_azimuth_diff = np.pi / 4
 
     # get lowest known area ratio that is greater than min_known_area_ratio
     candidates = [(i, known_area_ratio[i]) for i in range(seq_info.length) if known_area_ratio[i] >= min_known_area_ratio and seq_info.views[i].stage == -1]
     candidates.sort(key=lambda x: x[1])
     
-    generate_idx = [candidates[i][0] for i in range(min(generate_cnt, len(candidates)))]
-
-    if len(generate_idx) < generate_cnt:
+    if len(candidates) < generate_cnt:
         additional_candidates = [(i, known_area_ratio[i]) for i in range(seq_info.length) if known_area_ratio[i] < min_known_area_ratio and seq_info.views[i].stage == -1]
         additional_candidates.sort(key=lambda x: -x[1])
-        additional_generate_cnt = generate_cnt - len(generate_idx)
-        generate_idx += [additional_candidates[i][0] for i in range(additional_generate_cnt)]
+        additional_generate_cnt = generate_cnt - len(candidates)
+        candidates += additional_candidates[:additional_generate_cnt]
+
+    if len(candidates) == generate_cnt:
+        generate_idx = [candidates[i][0] for i in range(len(candidates))]
+    else: # len(candidates) > generate_cnt
+        generate_idx = []
+        reference_idx = [i for i in range(seq_info.length) if seq_info.views[i].stage != -1]
+        # print("Reference idx:" + str(reference_idx))
+
+        not_so_far_from_reference_idx = []
+        not_so_far_from_reference_info = []
+        for candidate_idx, candidate_info in enumerate(candidates):
+            min_azimuth = np.pi
+            for ref in reference_idx:
+                if seq_info.pair_angles[candidate_info[0], ref] < min_azimuth:
+                    min_azimuth = seq_info.pair_angles[candidate_info[0], ref]
+            if min_azimuth < max_azimuth_diff:
+                not_so_far_from_reference_idx.append(candidate_idx)
+                not_so_far_from_reference_info.append((min_azimuth, candidate_idx))
+        not_so_far_from_reference_info.sort(key=lambda x: -x[0])
+        # print("Not so far from reference info:")
+        # for info in not_so_far_from_reference_info:
+        #     print(f"  {info[0]} -> {candidates[info[1]][0]}")
+
+        while len(generate_idx) < generate_cnt:
+            if len(generate_idx) == 0:
+                # get max min_azimuth candidate
+                generate_idx.append(candidates[not_so_far_from_reference_info[0][1]][0])
+
+            else:
+                azimuth_from_known = []
+                for candidate_idx in not_so_far_from_reference_idx:
+                    candidate_info = candidates[candidate_idx]
+                    if candidate_idx in generate_idx:
+                        continue
+
+                    min_azimuth = np.pi
+                    for j in generate_idx + reference_idx:
+                        if seq_info.pair_angles[candidate_info[0], j] < min_azimuth:
+                            min_azimuth = seq_info.pair_angles[candidate_info[0], j]
+
+                    azimuth_from_known.append((candidate_idx, min_azimuth))
+
+                if len(azimuth_from_known) == 0:
+                    for i, candidate_info in enumerate(candidates):
+                        if i in generate_idx:
+                            continue
+
+                        min_azimuth = np.pi
+                        for j in generate_idx + reference_idx:
+                            if seq_info.pair_angles[candidate_info[0], j] < min_azimuth:
+                                min_azimuth = seq_info.pair_angles[candidate_info[0], j]
+                        azimuth_from_known.append((i, min_azimuth))
+
+                # get candidate with max min_azimuth
+                azimuth_from_known.sort(key=lambda x: -x[1])
+                max_min_azimuth_candidate_idx = azimuth_from_known[0][0]
+                generate_idx.append(candidates[max_min_azimuth_candidate_idx][0])
 
     print(f"Generate {generate_cnt} images:")
     for idx in generate_idx:
